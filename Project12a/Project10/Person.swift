ec8e3486333782ee7a396004cf67573c9f72b32b
tom8u4286 <tom8u4286@gmail.com>
2022-10-09 17:46:40 +0800

    新增 修改Project10至Project12a，以NSCoding的形式存入資料進UserDefault

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,39 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by 曲奕帆 on 2022/10/7.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+    
+    /** 此處我們預計將Person物件存入UesrDefault(App的基本DB)中，
+     * 而存入此DB必須follow NSCoding的protocol。
+     * 在準備存入此筆資料時，使用encoder，
+     * 要讀取此筆資料時，再用decoder解碼。
+     */
+    required init?(coder aDecoder: NSCoder){
+        let test = aDecoder.decodeObject(forKey: <#T##String#>)
+        /** 由於decodeObject轉換出來的格式為Any?，
+         * 轉換回String資料格式。
+         * 我們使用decodeObject是因為目前沒有decodeString的function
+         */
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
+}
---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by 曲奕帆 on 2022/10/6.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
