73bc73eb79b84de834bc99d213eb0425bc175648
tom8u4286 <tom8u4286@gmail.com>
2022-10-14 15:09:58 +0800

    新增 DetailViewController類別與storyboard中的DatailView，點選Cell後進入DetailView

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by 曲奕帆 on 2022/10/14.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 以FileManager 取出App中的檔案名稱
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
-
+    // numberOfRowsInSection 一個Section有多少Cell
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // cellForRowAt Cell要顯示什麼內容
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    // didSelectRowAt 當點選到某一個Cell時
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        /** 此處有三處可能失敗
+         *  1.storyboard可能不存在，因此要使用storyboard?
+         *  2.可能沒有id為"Detail"的view
+         *  3.可能無法轉換為DetailViewController
+         */
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+    
 }
 
