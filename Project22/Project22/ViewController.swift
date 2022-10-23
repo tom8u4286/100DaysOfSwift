6b3773c225567c869c4e0d27929ee0db3d15772a
tom8u4286 <tom8u4286@gmail.com>
2022-10-23 17:17:49 +0800

    完成 Project22，以指定編號搜尋臨近的iBeacon裝置

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by 曲奕帆 on 2022/10/21.
+//
+
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController,
+                    CLLocationManagerDelegate{
+
+    @IBOutlet var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        // 要求所有時間皆可取用User位置的權限
+        locationManager?.requestAlwaysAuthorization()
+        // 要求使用App時可以取用User位置的權限
+//        locationManager?.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    //
+    /** 當User同意位置權限時，會觸發本function
+     * 此處我們設計，當User同意我們使用位置權限為.authorizedAlways時，
+     * 即開始進行iBeacon的掃描(startScanning())。
+     */
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // 如果權限狀態為「永遠」
+        if manager.authorizationStatus == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                // 檢查這隻手機是否可以判斷iBeacon與手機的距離
+                if CLLocationManager.isRangingAvailable(){
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    /** 開始以我們定義的iBeacon編號進行搜尋
+     *  iBeacon以三樣編號將其定義為Unique
+     *  1.UUID
+     *  2.Majon
+     *  3.Minor
+     */
+    func startScanning(){
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+        
+    }
+    
+    /** 在蘋果的iBeacon架構中，定義出了四種不同的距離
+     * 1. unknown  遠於50M
+     * 2. far 約50M以內
+     * 3. near 約2M以內
+     * 4. immediate 約0.5M以內
+     */
+    func update(distance: CLProximity){
+        UIView.animate(withDuration: 1) {
+            switch distance {
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceReading.text = "FAR"
+            
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceReading.text = "NEAR"
+                
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceReading.text = "RIGHT HERE"
+                
+            default:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            }
+        }
+    }
+    
+    /** 當收到beacon的距離時，觸發此function
+     * App將更新畫面顏色及Label文字
+     */
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if let beacon = beacons.first {
+            // 更新畫面
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
+
+
+}
+
