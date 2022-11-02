f18d12c88ad1d6549a13c13b55486d93b08b5672
tom8u4286 <tom8u4286@gmail.com>
2022-11-02 10:15:54 +0800

    完成 Project28，使用TouchID與FaceID解鎖文件

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
@@ -14,12 +15,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         // 建立center，通知我們鍵盤的狀態
         let notificationCenter = NotificationCenter.default
+        
+        // 監聽鍵盤準備隱藏時
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // 監聽鍵盤準備出現時
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        // 監聽app準備進入inactive狀態時(離開前景)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
     
+    // 調整TextInput的位置，讓文字不要被鍵盤擋住。
     @objc func adjustForKeyboard(notification: Notification){
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         
@@ -36,12 +45,72 @@ class ViewController: UIViewController {
              * 因此要將homeIndicator的高度扣掉。
              */
             secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
-            
         }
+
+        // scrollIndicator(右側滑動桿)也同樣要上移
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
     }
 
+    // 當點選按鈕時，進行touchID或faceID驗證
     @IBAction func authenticateTapped(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError?
         
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+                /** 由於User在執行FaceID或是TouchID時會需要時間，
+                 * 因此在驗證執行完後，要用DispatchQueue將後續的工作推回UI。
+                 */
+                DispatchQueue.main.async {
+                    // 如果User FaceID 成功
+                    if success {
+                        // 解除鎖定
+                        self?.unlockSecretMessage()
+                    // 如果User驗證失敗
+                    } else {
+                        let ac = UIAlertController(title: "驗證失敗", message: "Authentication failed. Please try again!", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        // 如果這個裝置沒有TouchID或FaceID
+        } else {
+            let ac = UIAlertController(title: "不支援生物識別", message: "你的裝置沒有FaceID或TouchID", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    // 將密文轉換為明文
+    func unlockSecretMessage(){
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        // 將文字用KeychainWrapper包裝
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+    }
+    
+    // 將明文轉為密文
+    @objc func saveSecretMessage(){
+        guard secret.isHidden == false else { return }
+        
+        // 將明文存入Keychain之中
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        
+        /** 通知我們應該要離開focus的狀態(請系統離開編輯狀態)。
+         *
+         * 教材說明：
+         * https://www.hackingwithswift.com/read/28/3/writing-somewhere-safe-the-ios-keychain
+         */
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
     }
     
 }
